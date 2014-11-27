8a9aabbf85969f765bc85baba6d79508fba55dc3
Neel Shah <nshah@sprinklr.com>
2014-11-27 22:23:28 +0530

    initial commit

---
+++ Coding Practice/PlayAround.h
@@ -0,0 +1,14 @@
+//
+//  PlayAround.h
+//  Coding Practice
+//
+//  Created by Neel Shah on 11/27/14.
+//  Copyright (c) 2014 Neel Shah. All rights reserved.
+//
+
+#ifndef __Coding_Practice__PlayAround__
+#define __Coding_Practice__PlayAround__
+
+#include <stdio.h>
+
+#endif /* defined(__Coding_Practice__PlayAround__) */
---
+++ Coding Practice/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  Coding Practice
+//
+//  Created by Neel Shah on 11/27/14.
+//  Copyright (c) 2014 Neel Shah. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+#include <string>
+#include <map>
+#include<stack>
+#include<map>
+#include<queue>
+#include <math.h>
+#include<set>
+#include<stdint.h>
+#include <utility>
+#define MAXN 100005
+using namespace std;
+typedef long long int ll;
+
+int main() {
+    printf("hello");
+    return 0;
+}
